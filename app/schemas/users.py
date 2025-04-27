98def9f621d574df57599b38ccbdebae7c0371f9
idalz <idalez@outlook.com>
2025-04-28 00:31:21 +0300

    Create new schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,6 +1,6 @@
 
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, List
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -15,3 +15,27 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class UserInfoSchema(BaseModel):
+    email: EmailStr
+    name: str
+    phone_number: Optional[str] = ""
+    linkedin_url: Optional[str] = ""
+    github_url: Optional[str] = ""
+    resume: Optional[str] = ""
+    is_recruiter: Optional[bool] = False
+
+    class ConfigDict:
+        from_attributes = True   
+
+class EmailsRequest(BaseModel):
+    emails: List[EmailStr]
+
+class UserUpdateSchema(BaseModel):
+    name: str
+    phone_number: Optional[str] = ""
+    linkedin_url: Optional[str] = ""
+    github_url: Optional[str] = ""
+
+    class ConfigDict:
+        from_attributes = True   
